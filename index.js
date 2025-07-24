345ad7cc075907c05ffd09791bea4c133e3a01e4
chrisdiazaguilar <chrisdiaz0641@gmail.com>
2025-07-24 09:46:52 -0400

    updated CSS file, JavaScript

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
-let passOne = document.getElementById("passwordOne")
-let passTwo = document.getElementById("passwordTwo")
+// Access the two password display elements
+let passOne = document.getElementById("passwordOne");
+let passTwo = document.getElementById("passwordTwo");
 
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+// All possible characters to use in passwords
+const characters = [
+  "A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P",
+  "Q","R","S","T","U","V","W","X","Y","Z",
+  "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p",
+  "q","r","s","t","u","v","w","x","y","z",
+  "0","1","2","3","4","5","6","7","8","9",
+  "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=",
+  "{","[","}","]",",","|",":",";","<",">",".","?","/"
+];
 
-function getRandomString() {
-  const characters = ["A", "B", "C", "D", "E", "F", "G"];
-  let result = "";
-
-  for (let i = 0; i < 5; i++) {
+// Function to generate a random password of a given length
+function generateRandomPassword(length) {
+  let password = "";
+  for (let i = 0; i < length; i++) {
     let randomIndex = Math.floor(Math.random() * characters.length);
-    result += characters[randomIndex];
+    password += characters[randomIndex];
   }
-
-  console.log(result);
+  return password;
 }
 
+// Main function that gets called when button is clicked
+function passwordGen() {
+  passOne.textContent = generateRandomPassword(15);
+  passTwo.textContent = generateRandomPassword(15);
+}
