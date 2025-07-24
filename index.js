97a721732a1dcb2707c4eb5426311ad415cea43c
Chris Diaz <chrisdiaz0641@gmail.com>
2025-07-24 08:36:00 -0400

    Updated CSS file

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ let passTwo = document.getElementById("passwordTwo")
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+function getRandomString() {
+  const characters = ["A", "B", "C", "D", "E", "F", "G"];
+  let result = "";
+
+  for (let i = 0; i < 5; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length);
+    result += characters[randomIndex];
+  }
+
+  console.log(result);
+}
+
